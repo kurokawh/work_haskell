436c3cba5ee77c4a9417038452baf220daea0a67
Hiroyuki Kurokawa <pxi05647@nifty.com>
2014-08-31 02:08:09 +0900

    - add comment to explain sample code.

--- yesodweb/persistent/code_generation.hs
+++ yesodweb/persistent/code_generation.hs
@@ -5,20 +5,35 @@
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
-import           Control.Monad.IO.Class  (liftIO)
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
+import Control.Monad.IO.Class  (liftIO)
+import Database.Persist         -- persistentパッケージ
+import Database.Persist.Sqlite  -- persistent-sqliteパッケージ
+import Database.Persist.TH      -- persistent-templateパッケージ
 
+-- mkPersist
+--   mkPersist :: MkPersistSettings -> [EntityDef SqlType] -> Q [Dec]
+--   データ型、PersistentEntityインスタンスを生成
+-- sqlSettings
+--   mkPersistの挙動を変える設定値
+-- mkMigrate "migrateAll"
+--   マイグレーション処理を定義
+-- QuansiQuotes [xx|..|] でスキーマ定義。
+--   persistentLowaCaseはテーブル名・フィールド名に、"_"区切り小文字を
+--   利用することを指示している。
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
-    name String
-    age Int Maybe
+Person             -- personテーブル
+    name String    -- nameカラム（VARCHAR, NOT NULL制約）
+    age Int Maybe  -- ageカラム（INTEGER, NULLあり）
     deriving Show
 |]
 
 main :: IO ()
-main = runSqlite ":memory:" $ do
+main = runSqlite ":memory:" $ do -- DBオープン時の引数。メモリDB利用。"test.db"等の
+                                 -- ファイル名を渡すとファイルDBがオープンされる。
+    -- name: "Michael", age: 26 のレコードを INSERT
+    -- id（row-id）が返値として返される。
     michaelId <- insert $ Person "Michael" $ Just 26
+    -- row-id でレコードを SELECT。該当レコードが返される。
     michael <- get michaelId
+    -- 返されたレコードを表示。
     liftIO $ print michael
