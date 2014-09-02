5b8b809bebabc0707f13ad9f5c80f69b4e328b33
Hiroyuki Kurokawa <pxi05647@nifty.com>
2014-09-03 00:14:44 +0900

    - separate some db operation to another fechingById func from main.   but I cannot declare type of that func yet...

---
+++ yesodweb/persistent/query.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE EmptyDataDecls    #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+import           Control.Monad.IO.Class  (liftIO)
+import           Database.Persist
+import           Database.Persist.Sqlite
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    firstName String
+    lastName String
+    age Int
+    PersonName firstName lastName
+    deriving Show
+|]
+
+--fechingById :: IO ()
+fechingById = do
+    personId <- insert $ Person "Michael2" "Snoyman2" 26
+    maybePerson <- get personId
+    case maybePerson of
+      Nothing -> liftIO $ putStrLn "Just kidding, not really there"
+      Just person -> liftIO $ print person
+
+main :: IO ()
+main = runSqlite ":memory:" $ do
+    runMigration migrateAll
+
+    personId <- insert $ Person "Michael" "Snoyman" 26
+    maybePerson <- get personId
+    case maybePerson of
+      Nothing -> liftIO $ putStrLn "Just kidding, not really there"
+      Just person -> liftIO $ print person
+
+    x <- fechingById
+    return x
