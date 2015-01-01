ed7b991488bdfc139e2e31be263896effdd9d820
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-01-01 11:16:05 +0900

    - error because of same problem of needing a pure value.

---
+++ etc/monad/LTMT/part3/bind.hs
@@ -0,0 +1,10 @@
+{-
+LTMT Part 3: The Monad Cookbook
+http://softwaresimply.blogspot.jp/2014/12/ltmt-part-3-monad-cookbook.html
+-}
+
+import System.Environment
+main :: IO ()
+main = do
+    [from,to] <- getArgs
+    writeFile to $ readFile from
