69d48f3180fc3888c55a9bfc0a562aef93d03da3
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-02-22 21:51:21 +0900

    - link persistent libs to refer sqlite.

--- csv2db/Main.hs
+++ csv2db/Main.hs
@@ -1,35 +1,47 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 import System.Console.CmdArgs
 import Database.Persist
 import Database.Persist.Sqlite
 import Database.Persist.TH
+import Data.Text (pack)
 import MyArgs
 
 {-
+ removed LANGUAGE opt:
+   FlexibleContexts 
+   OverloadedStrings 
+   DeriveDataTypeable 
+   EmptyDataDecls   
+-}
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
-    name String
-    age Int
-    gender Gender -- Genderカラム（VARCHAR: Male or Female）
-    Name name     -- nameカラムにUnique制約を付与
+    serverTime String -- Int?
+    consoleType Int
+    systemVer Int
+    productCode Int
+    productSubCode Int
+    idu Bool
+    logConfVer String
+    timestamp String -- Int?
+    clockType Int
+    ps4UniqueId String
+    p1 String
+    p2 String
+    p3 String
+    p4 String
+    p5 String
+    p6 String
+    p7 String
+    p8 String
+    p9 String
+    p10 String
     deriving Show
-| ]
--}
-mkPersist sqlSettings [persistLowerCase|
-  Parent
-      name  String maxlen=20
-      name2 String maxlen=20
-      age Int
-      Primary name name2 age
-      deriving Show Eq
-  Child
-      name  String maxlen=20
-      name2 String maxlen=20
-      age Int
-      Foreign Parent fkparent name name2 age
-      deriving Show Eq
 |]
-
 
 dispatch :: [(String, MyArgs -> IO ())]
 dispatch =  [ ("sqlite", to_sqlite)
@@ -37,7 +49,8 @@ dispatch =  [ ("sqlite", to_sqlite)
 --            , ("mysql", to_mysql)
             ]
 
-to_sqlite args = do
+to_sqlite args = runSqlite (pack $ targetdb args) $ do
+  runMigration migrateAll
   return ()
 
 main = do
