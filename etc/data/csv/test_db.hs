10b94279f17b0626e455b7140ed43d890d66b4d5
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-02-24 23:43:16 +0900

    - 9 is OK by deafult. - 10 & 20 were NG by default. - define new instance for 10.csv. -- though otherwise could not be implemented well.

--- etc/data/csv/test_db.hs
+++ etc/data/csv/test_db.hs
@@ -5,11 +5,75 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 
+--import Data.Csv.Incremental ( Parser(..) )
+import Control.Applicative (Alternative, Applicative, (<*>), (<$>), (<|>),
+                            (<*), (*>), empty, pure)
+
 data Salary = Salary {
       name :: String
     , salary :: Int
 }     deriving Show
 
+type Row20 = (String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String)
+
+type Row9  = (String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String)
+
+type Row10 = (String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String, 
+              String)
+
+instance (FromField a, FromField b, FromField c, FromField d, FromField e,
+          FromField f, FromField g, FromField h, FromField i, FromField j) =>
+         FromRecord (a, b, c, d, e, f, g, h, i, j) where
+    parseRecord v
+        | n == 10  = (,,,,,,,,,) <$> unsafeIndex v 0
+                                 <*> unsafeIndex v 1
+                                 <*> unsafeIndex v 2
+                                 <*> unsafeIndex v 3
+                                 <*> unsafeIndex v 4
+                                 <*> unsafeIndex v 5
+                                 <*> unsafeIndex v 6
+                                 <*> unsafeIndex v 7
+                                 <*> unsafeIndex v 8
+                                 <*> unsafeIndex v 9
+--        | otherwise = lengthMismatch 10 v
+--        | otherwise = (Fail "a" "b") --Done [Left "x"] 
+          where
+            n = V.length v
 
 --to_sal :: (String, Int) -> Salary
 to_sal (x, y) = Salary x y
@@ -25,5 +89,7 @@ main = do
         Right v -> do
 --          V.mapM_ (putStrLn.show) (v2v v)
 --          V.mapM_ (putStrLn.show) (v :: (V.Vector (String, Int)))
-          V.mapM_ (putStrLn.show) (v :: (V.Vector (String, String)))
+--          V.mapM_ (putStrLn.show) (v :: (V.Vector (String, String)))
+          V.mapM_ (putStrLn.show) (v :: (V.Vector Row10))
+--          V.mapM_ (putStrLn.show) (v :: (V.Vector Row9))
           putStrLn ""
