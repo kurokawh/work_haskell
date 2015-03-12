5486d4aaa853db2a15d5cc075df53ad2f03fdf6b
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-03-13 02:07:04 +0900

    - construct Person from csv data.

--- etc/data/csv/test_db.hs
+++ etc/data/csv/test_db.hs
@@ -7,6 +7,7 @@ import Data.Csv
 import qualified Data.Vector as V
 
 --import Data.Csv.Incremental ( Parser(..) )
+import Control.Monad (MonadPlus, mplus, mzero)
 import Control.Applicative (Alternative, Applicative, (<*>), (<$>), (<|>),
                             (<*), (*>), empty, pure)
 
@@ -20,102 +21,45 @@ data Salary = Salary {
     , salary :: Int
 }     deriving Show
 
-type Row20 = (String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String)
 
-type Row9  = (String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String)
-
-type Row10 = (String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String, 
-              String)
-
-{-                
-instance (FromField a, FromField b, FromField c, FromField d, FromField e,
-          FromField f, FromField g, FromField h, FromField i, FromField j) =>
-         FromRecord (a, b, c, d, e, f, g, h, i, j) where
+instance FromRecord Person where
     parseRecord v
-        | n == 10  = (,,,,,,,,,) <$> unsafeIndex v 0
-                                 <*> unsafeIndex v 1
-                                 <*> unsafeIndex v 2
-                                 <*> unsafeIndex v 3
-                                 <*> unsafeIndex v 4
-                                 <*> unsafeIndex v 5
-                                 <*> unsafeIndex v 6
-                                 <*> unsafeIndex v 7
-                                 <*> unsafeIndex v 8
-                                 <*> unsafeIndex v 9
--}
-instance FromRecord Salary where
-    parseRecord v
-        | V.length v == 10 = Salary <$>
+        | n == 10 = Person <$>
                           v .! 0 <*>
                           v .! 1
-
---        | otherwise = lengthMismatch 10 v
---        | otherwise = (Fail "a" "b") --Done [Left "x"] 
+        | n >= 11 = Person <$>
+                          v .! 9 <*>
+                          v .! 10
+        | otherwise     = mzero
           where
             n = V.length v
 
---to_sal :: (String, Int) -> Salary
-to_sal (x, y) = Salary x y
+--to_sal :: (String, Int) -> Person
+to_sal (x, y) = Person x y
 
 v2v v = V.map to_sal v
 
 -- currently only following command is supported:
 -- % runghc test_db.hs 10.csv
-main2 :: IO ()
-main2 = do
+main1 :: IO ()
+main1 = do
     (filename:args) <- getArgs  
     csvData <- BL.readFile filename
     case decode NoHeader csvData of
         Left err -> putStrLn err
         Right v -> do
---          V.mapM_ (putStrLn.show) (v2v v)
---          V.mapM_ (putStrLn.show) (v :: (V.Vector (String, Int)))
---          V.mapM_ (putStrLn.show) (v :: (V.Vector (String, String)))
---          V.mapM_ (putStrLn.show) (v :: (V.Vector Row10))
-          V.mapM_ (putStrLn.show) (v :: (V.Vector Salary))
---          V.mapM_ (putStrLn.show) (v :: (V.Vector Row9))
+          V.mapM_ (putStrLn.show) (v :: (V.Vector Person))
           putStrLn ""
 
 -- copied from YesodPerson.hs.
-main :: IO ()
-main = runSqlite ":memory:" $ do
+main2 :: IO ()
+main2 = runSqlite ":memory:" $ do
     -- this line added: that's it!
     runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Person)
     michaelId <- insert $ Person "Michael" 26
     michael <- get michaelId
     liftIO $ print michael
+
+
+main :: IO ()
+main = main1
