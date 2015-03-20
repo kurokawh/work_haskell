d06c300f2725d5635bc7b352125b30a5a381673e
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2015-03-20 15:42:53 +0900

    - no file or parse error is shown as expected. ToDo: remove unnecessary code & debug print code.

--- etc/data/csv/test_db.hs
+++ etc/data/csv/test_db.hs
@@ -8,27 +8,40 @@ import qualified Data.Vector as V
 
 --import Data.Csv.Incremental ( Parser(..) )
 --import Control.Monad (MonadPlus, mplus, mzero)
---import Control.Applicative (Alternative, Applicative, (<*>), (<$>), (<|>),
---                            (<*), (*>), empty, pure)
+import Control.Applicative (Alternative, Applicative, (<*>), (<$>), (<|>),
+                            (<*), (*>), empty, pure)
 
 import Database.Persist
 import Database.Persist.Sqlite
 import Control.Monad.IO.Class (liftIO)
 import YesodPerson
-    
+
+import System.IO
 
 
 file_to_vec :: String -> IO (V.Vector Person)
 file_to_vec filename = do
+    putStrLn "call openFile"
+    h <- openFile filename ReadMode
+    putStrLn "ret openFile"
+    hClose h
+    --putStrLn "ret hGetContents"
+    putStrLn ("call ReadFile" ++ filename)
     csvData <- BL.readFile filename
+    putStrLn "ret ReadFile"
     case decode NoHeader csvData of
-        Left err -> return V.empty
-        Right v -> return v
+        Left err -> do
+          putStrLn err
+          error err
+        Right v -> do
+          --putStrLn "OK!"
+          return v
                 
 -- multiple argument (csv files) are supported
 -- % runghc test_db.hs 10.csv x.csv y.csv ..
 main :: IO ()
 main = do
+  putStrLn "start"
   args <- getArgs
   vlist <- mapM file_to_vec args
   runSqlite "x.db" $ do
