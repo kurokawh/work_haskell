521c22537ae0f5602e01650616df908ffcb899df
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2015-03-20 15:34:28 +0900

    - still returns error even though csv is merged. DB is not merged yet.

--- etc/file/open.hs
+++ etc/file/open.hs
@@ -2,15 +2,45 @@ import System.Environment
 import System.IO
 
 import qualified Data.Vector as V
+import Data.Csv
+import qualified Data.ByteString.Lazy as BL
+import Control.Applicative (Alternative, Applicative, (<*>), (<$>), (<|>),
+                            (<*), (*>), empty, pure)
 
-test_open :: String -> IO (V.Vector a)
+data Person = Person { 
+      name :: !String, age :: !Int 
+}  deriving (Eq, Ord, Show)
+instance FromRecord Person where
+    parseRecord v
+        | V.length v == 2 = Person <$>
+                          v .! 0 <*>
+                          v .! 1
+        | V.length v == 10 = Person <$>
+                          v .! 0 <*>
+                          v .! 1
+        | V.length v == 20 = Person <$>
+                          v .! 18 <*>
+                          v .! 19
+    
+test_open :: String -> IO (V.Vector Person)
 test_open file = do
   putStrLn "test_open"
   handle <- openFile file ReadMode
   contents <- hGetContents handle
   putStr contents
   hClose handle
-  error ""
+  --error ""
+  putStrLn ("call ReadFile" ++ file)
+  csvData <- BL.readFile file
+  --csvData <- readFile file
+  putStrLn "ret ReadFile"
+  case decode NoHeader csvData of
+        Left err -> do
+          putStrLn err
+          error err
+        Right v -> do
+          --putStrLn "OK!"
+          return v
 
 -- following error returns as explected if file is not existed.
 -- open.hs xxx: openFile: does not exist (No such file or directory)
