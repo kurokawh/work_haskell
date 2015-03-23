cb49dec87b65c822ba17e8db6124b1fab46a8d51
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2015-03-23 15:29:29 +0900

    - following command works now! : > csv2db x.db 20.csv

--- csv2db/Main.hs
+++ csv2db/Main.hs
@@ -1,35 +1,56 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 import System.Console.CmdArgs
+import qualified Data.ByteString.Lazy as BL
+import Data.Text (pack)
+import qualified Data.Vector as V
+import qualified Data.Csv as C
 import Database.Persist
 import Database.Persist.Sqlite
-import Data.Text (pack)
+
 import MyArgs
 import Telemetry
 
 
-dispatch :: [(String, MyArgs -> IO ())]
+--dispatch :: [(String, MyArgs -> IO ())] -- ToDo: remove 2nd argument (vlist).
 dispatch =  [ ("sqlite", to_sqlite)
 --            , ("postgresql", to_postgresql)
 --            , ("mysql", to_mysql)
             ]
 
 
-files_to_telemetries :: [String] -> [Telemetry]
-files_to_telemetries csvfiles = 
-    [
-     Telemetry "20150213103400" 1 2501030 385 16 False "20150109-01" "2015021310290298" 2 "097a7e482eec53225e524ddfa2c4b5b755b7eceaa507e1ac3c089ca816686ceb" "" "" "" "" "" "" "" "" "" ""
-    ]
 
-
-
-to_sqlite args = runSqlite (pack $ targetdb args) $ do
+-- ToDo remove 2nd argument (vlist).
+to_sqlite args vlist = runSqlite (pack $ targetdb args) $ do
   runMigration migrateAll
-  mapM insert (files_to_telemetries (csvfiles args))
+--  mapM insert (files_to_telemetries (csvfiles args))
+  ids <- mapM (V.mapM insert) vlist
+--  liftIO $ print ids
   return ()
+
+
+
+
+file_to_vec :: String -> IO (V.Vector Telemetry)
+file_to_vec filename = do
+    putStrLn ("parsing : " ++ filename)
+    csvData <- BL.readFile filename
+    case C.decode C.NoHeader csvData of
+        Left err -> do
+          putStrLn err
+          error err
+        Right v -> do
+          --putStrLn "OK!"
+          return v
+
+arg_to_vlist :: MyArgs -> IO [V.Vector Telemetry]
+arg_to_vlist args = do
+  vlist <- mapM file_to_vec (csvfiles args)
+  return vlist
 
 main = do
   args <- cmdArgs config
-  let (Just register) = lookup (dbopt　args) dispatch
+  let (Just to_db) = lookup (dbopt　args) dispatch
+  vlist <- arg_to_vlist args
   -- print args
-  register args
+  to_db args vlist
--- csv2db/Telemetry.hs
+++ csv2db/Telemetry.hs
@@ -9,6 +9,11 @@ module Telemetry
     , migrateAll
     ) where
 
+import Control.Monad (MonadPlus, mplus, mzero)
+import Control.Applicative (Alternative, Applicative,
+                            (<*>), (<$>), (<|>), (<*), (*>), empty, pure)
+import qualified Data.Vector as V
+import Data.Csv
 import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
@@ -18,7 +23,7 @@ Telemetry
     systemVer Int
     productCode Int
     productSubCode Int
-    idu Bool
+    idu Int --Bool
     logConfVer String
     timestamp String -- Int?
     clockType Int
@@ -33,7 +38,30 @@ Telemetry
     p8 String
     p9 String
     p10 String
-    Primary serverTime
+--    Primary serverTime
     deriving Show Eq
 |]
     
+instance FromRecord Telemetry where
+    parseRecord v = Telemetry <$>
+                          v .! 0 <*>
+                          v .! 1 <*>
+                          v .! 2 <*>
+                          v .! 3 <*>
+                          v .! 4 <*>
+                          v .! 5 <*>
+                          v .! 6 <*>
+                          v .! 7 <*>
+                          v .! 8 <*>
+                          v .! 9 <*>
+                          v .! 10 <*>
+                          v .! 11 <*>
+                          v .! 12 <*>
+                          v .! 13 <*>
+                          v .! 14 <*>
+                          v .! 15 <*>
+                          v .! 16 <*>
+                          v .! 17 <*>
+                          v .! 18 <*>
+                          v .! 19
+
