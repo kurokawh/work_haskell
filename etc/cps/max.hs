e1fa491ab34c98a8410c6d06047d70fafe8990b9
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-05-10 18:08:14 +0900

    - add CPS version of max function.

---
+++ etc/cps/max.hs
@@ -0,0 +1,24 @@
+-- 再帰関数のスタックオーバーフローを倒す話 その1
+-- http://bleis-tift.hatenablog.com/entry/cps
+-- おまけ(2)
+
+-- オリジナル
+my_max [x] = x
+my_max (x:xs) =
+    let pre = my_max xs
+    in if pre < x
+       then
+           x
+       else
+           pre
+
+
+cps_max [x] cont = cont x
+cps_max (x:xs) cont = 
+    cps_max xs (\pre ->
+                if pre < x
+                then
+                    cont x
+                else
+                    cont pre)
+                             
