9e969c751a123693f0765327927a8b66771288bf
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-07-05 15:53:10 +0900

    - is this OK? net answer is listed as well.

--- etc/cps/map.hs
+++ etc/cps/map.hs
@@ -14,13 +14,20 @@ my_map2 f (x:xs) =
     in y:ys
 
 -- CPS
+--cps_map :: (a -> b) -> [a] -> ([b] -> [b]) -> [b]
 cps_map _ [] cont = cont []
-cps_map f (x:xs) cont = 
-    -- let y = f x
-    -- in cps_map f xs (\ys -> (y : ys))
-    cps_map f xs (\ys -> (f x) : ys)
+cps_map f (x:xs) cont =
+--    let y = f x
+--    in cps_map f xs (cont (\ys -> (y : ys)))
+--    cps_map f xs (cont (\ys -> ((f x) : ys)))
+    cont (cps_map f xs (\ys -> ((f x) : ys)))
 
 {- ???
-> cps_map ((+) 1) [1,2] id
-[3]
+> cps_map ((+) 1) [1,2,3] id
+> cps_map ((+) 1) [2,3] (id (\fs -> 2 : ys))
 -}
+
+-- another answer:
+cpsMap :: (a -> b) -> [a] -> ([b] -> r) -> r
+cpsMap f (a:as) c = cpsMap f as (c.(f a:))
+cpsMap  _ []    c = c []
