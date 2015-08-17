9bdc80ed1a34e45266c8ff0065c36d559c678f92
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-08-18 01:15:33 +0900

    - remove warning of hlint.

--- etc/string/string_to_int.hs
+++ etc/string/string_to_int.hs
@@ -5,7 +5,7 @@ main = do
   (arg:xs) <- getArgs
   let i = read arg :: Int -- convert decimal string to int. Note "::Int" is needed
   let [(x,_)] = readHex arg -- convert from hex string to int
-  putStrLn ("readInt: " ++ (show i))
-  putStrLn ("readHex: " ++ (show x))
+  putStrLn ("readInt: " ++ show i)
+  putStrLn ("readHex: " ++ show x)
   return ()
 
