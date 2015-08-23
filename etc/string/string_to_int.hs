6eb967bb5bd627a952415fd336458b813925a0f6
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-08-23 11:37:41 +0900

    - remove 2 warnings.

--- etc/string/string_to_int.hs
+++ etc/string/string_to_int.hs
@@ -1,8 +1,9 @@
 import System.Environment
 import Numeric
 
+main :: IO ()
 main = do
-  (arg:xs) <- getArgs
+  (arg:_) <- getArgs
   let i = read arg :: Int -- convert decimal string to int. Note "::Int" is needed
   let [(x,_)] = readHex arg -- convert from hex string to int
   putStrLn ("readInt: " ++ (show i))
