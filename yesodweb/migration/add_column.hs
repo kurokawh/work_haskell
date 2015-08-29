529cd2624c00652391c2fc4ee14205eb4bb27aa1
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-08-29 15:20:25 +0900

    - add non null column sapmple.

--- yesodweb/migration/add_column.hs
+++ yesodweb/migration/add_column.hs
@@ -12,18 +12,18 @@ import Database.Persist.TH
 import Database.Persist.Sqlite
 import Control.Monad.IO.Class (liftIO)
 
-share [mkPersist sqlSettings, mkSave "entityDefs"] [persistLowerCase|
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
     name String
     age Int
-    address String
+    address String  -- add new NON NULL column
     deriving Show
 |]
 
 main :: IO ()
 main = runSqlite "old.db" $ do
     -- this line added: that's it!
-    runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Person)
+    runMigration migrateAll
     michaelId <- insert $ Person "Michael" 26 "Tokyo"
     michael <- get michaelId
     liftIO $ print michael
