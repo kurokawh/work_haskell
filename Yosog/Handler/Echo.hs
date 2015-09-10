4370a61944da3ec585596195ad2158dac67fc6eb
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-09-11 01:26:56 +0900

    - remove test/Yosog to merge github/Yosog.

--- Yosog/Handler/Echo.hs
+++
@@ -1,7 +0,0 @@
-module Handler.Echo where
-
-import Import
-
-getEchoR :: Text -> Handler Html
---getEchoR = error "Not yet implemented: getEchoR"
-getEchoR theText = defaultLayout $(widgetFile "echo")
