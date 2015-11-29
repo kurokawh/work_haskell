f0f3d9d3cb48cd50e3e40bc18687973e4248b3a0
Hiroyuki Kurokawa <pxi05647@nifty.com>
2015-11-29 23:40:26 +0900

    - remove unnecessary comments.

--- yesodweb/persistent/create_index.hs
+++ yesodweb/persistent/create_index.hs
@@ -12,36 +12,21 @@ import Database.Persist         -- persistentパッケージ
 import Database.Persist.Sqlite  -- persistent-sqliteパッケージ
 import Database.Persist.TH      -- persistent-templateパッケージ
 
--- mkPersist
---   mkPersist :: MkPersistSettings -> [EntityDef SqlType] -> Q [Dec]
---   データ型、PersistentEntityインスタンスを生成
--- sqlSettings
---   mkPersistの挙動を変える設定値
--- mkMigrate "migrateAll"
---   マイグレーション処理を定義
--- QuansiQuotes [xx|..|] でスキーマ定義。
---   persistentLowaCaseはテーブル名・フィールド名に、"_"区切り小文字を
---   利用することを指示している。
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person             -- personテーブル。プライマリキーとして"id"カラム自動生成。
-    name String    -- nameカラム（VARCHAR, NOT NULL制約）
-    age Int Maybe  -- ageカラム（INTEGER, MaybeはNULLを許容することを意味する）
+Person
+    name String
+    age Int Maybe
     deriving Show
 |]
 
 main :: IO ()
-main = runSqlite "test.db" $ do -- DBオープン時の引数。メモリDB利用。"test.db"等の
-                                -- ファイル名を渡すとファイルDBがオープンされる。
+main = runSqlite "test.db" $ do
     -- personテーブル生成
     runMigration migrateAll
     -- nameカラムにindexを生成する。placeholderがないため第2引数は[]でよい。
     -- 2回目以降、すでにインデックスが存在している場合のためにIF NOE EXISTSが必要。
-    rawExecute "CREATE INDEX IF NOT EXISTS idx_name_on_person ON person(name);" []
+    rawExecute "CREATE INDEX IF NOT EXISTS idx_name_on_person ON person(name);" [] -- here!
 
-    -- name: "Michael", age: 26 のレコードを INSERT
-    -- id（プライマリキー）が返値として返される。
     michaelId <- insert $ Person "Michael" $ Just 26
-    -- id でレコードを SELECT。該当レコードが返される。
     michael <- get michaelId
-    -- 返されたレコード（Personインスタンス）を表示。
     liftIO $ print michael
