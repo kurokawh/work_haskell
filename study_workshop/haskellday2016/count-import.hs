afa7a759f494ca82e4770d37ea5941589a7310ee
Hiroyuki Kurokawa <pxi05647@nifty.com>
2016-09-17 21:59:10 +0900

    count import works now.

---
+++ study_workshop/haskellday2016/count-import.hs
@@ -0,0 +1,22 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package turtle
+
+{-# LANGUAGE OverloadedStrings #-}
+import Turtle
+import Data.Text (unpack)
+
+callgrep shellfile = do
+  view shellfile
+--  return shellfile
+  f <- shellfile
+  input (fromString $ unpack f) & grep (prefix "import")
+
+--countimport callfind = empty & inshell callfind & grep (prefix "import") & shell "wc -l"
+countimport callfind = empty & inshell callfind & callgrep & shell "wc -l"
+
+main = do
+  args <- arguments
+  let src = head args
+  let callfind = format ("find "%s%" -name \\*.hs") src
+  countimport callfind
+  
