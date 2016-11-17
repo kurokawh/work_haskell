0351fc39642feb4e03cd02d5bef048ff36700fc0
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2016-11-17 13:24:34 +0900

    Update comments.

--- etc/system/file/binary.hs
+++ etc/system/file/binary.hs
@@ -8,15 +8,16 @@ import qualified Data.ByteString as BS
 import qualified Data.Word as W
 
     
-
--- following error returns as explected if file is not existed.
--- open.hs xxx: openFile: does not exist (No such file or directory)
+-- binary file operation sample.
+-- returns error if 2 files are not given.
+-- % binary from to
+--   from: file to read.
+--     to: file to write (2 8-byte blocks are read from 'from' file).
 main = do
   (from:to:xs) <- getArgs
   putStrLn ("from: " ++ from)
   putStrLn ("to: " ++ to)
-  contents <- BS.readFile from
+  contents <- BS.readFile from -- BS.readFile :: FilePath -> IO BS.ByteString
   print (BS.unpack contents)
-  let x = BS.unpack contents
---  BS.writeFile to ((take 8 (drop 73 contents)) ++ (take 8 (drop 51 contents)))
+  let x = BS.unpack contents   -- BS.unpack :: BS.ByteString -> [W.Word8]
   BS.writeFile to (BS.pack ((take 8 (drop 73 x)) ++ (take 8 (drop 51 x))))
