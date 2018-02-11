8d334681d5037e6bdbf039a20cf6df4aa3f04215
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-02-11 22:53:39 +0900

    add url of original blog post.

--- etc/system/environment/envvar.hs
+++ etc/system/environment/envvar.hs
@@ -5,3 +5,5 @@ import System.Environment
 main = do
         buff <- getEnv "LANG"
         putStrLn buff
+
+-- http://capm-network.com/?tag=Haskell-%E7%92%B0%E5%A2%83%E5%A4%89%E6%95%B0%E3%81%AE%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B
