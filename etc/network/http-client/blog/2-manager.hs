b4afc8b44e6dba64456aa274f1de603c8c01042f
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-02-27 01:10:51 +0900

    sample to create ManagerSettings by myself with Internal package.

--- etc/network/http-client/blog/2-manager.hs
+++ etc/network/http-client/blog/2-manager.hs
@@ -1,57 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.HTTP.Client
 import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+
+import Network.HTTP.Client.Internal
+--mySettings = ManagerSettings { managerResponseTimeout = responseTimeoutMicro 60000000 }
+mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 60000000 }
 
 main :: IO ()
 main = do
-  manager <- newManager defaultManagerSettings
-
-  initialRequest <- parseRequest "http://httpbin.org/headers"
-  let request = initialRequest
-          { method = "GET"
---          , requestBody = ""
-          , requestHeaders =
-              [ ("User-Agent", "New Agent!")
-              ]
-          }
-  
+--  manager <- newManager tlsManagerSettings
+--  manager <- newManager $ managerSetProxy (proxyEnvironment Nothing) tlsManagerSettings
+  manager <- newManager mySettings
+--  let manager = managerTls { managerResponseTimeout = 60000000 }
+--  manager <- newManager  tlsManagerSettings { managerResponseTimeout = 60000000 }
+    
+  request <- parseRequest "https://httpbin.org/get"
   response <- httpLbs request manager
 
   putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
   print $ responseBody response
-
-
-{--
-#!/usr/bin/env stack
--- stack script --resolver lts-8.22
-{-# LANGUAGE OverloadedStrings #-}
-import           Data.Aeson                 (encode, object, (.=))
-import qualified Data.ByteString.Lazy.Char8 as L8
-import           Network.HTTP.Client
-import           Network.HTTP.Client.TLS
-import           Network.HTTP.Types.Status  (statusCode)
-
-main :: IO ()
-main = do
-    manager <- newManager tlsManagerSettings
-
-    -- Create the request
-    let requestObject = object
-            [ "name" .= ("Alice" :: String)
-            , "age"  .= (35 :: Int)
-            ]
-    initialRequest <- parseRequest "http://httpbin.org/post"
-    let request = initialRequest
-            { method = "POST"
-            , requestBody = RequestBodyLBS $ encode requestObject
-            , requestHeaders =
-                [ ("Content-Type", "application/json; charset=utf-8")
-                ]
-            }
-
-    response <- httpLbs request manager
-    putStrLn $ "The status code was: "
-            ++ show (statusCode $ responseStatus response)
-    L8.putStrLn $ responseBody response
-    
---}
