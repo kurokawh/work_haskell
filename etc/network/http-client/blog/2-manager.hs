213b209d3b3647745ef03ed1192c702faa5def8a
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-02-27 01:54:54 +0900

    Add proxy setting variations.

--- etc/network/http-client/blog/2-manager.hs
+++ etc/network/http-client/blog/2-manager.hs
@@ -4,16 +4,17 @@ import Network.HTTP.Types.Status (statusCode)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 
 import Network.HTTP.Client.Internal
---mySettings = ManagerSettings { managerResponseTimeout = responseTimeoutMicro 60000000 }
 mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 60000000 }
+--mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 5000000 }
 
 main :: IO ()
 main = do
 --  manager <- newManager tlsManagerSettings
---  manager <- newManager $ managerSetProxy (proxyEnvironment Nothing) tlsManagerSettings
-  manager <- newManager mySettings
---  let manager = managerTls { managerResponseTimeout = 60000000 }
---  manager <- newManager  tlsManagerSettings { managerResponseTimeout = 60000000 }
+  
+--  manager <- newManager $ managerSetProxy noProxy mySettings
+--  manager <- newManager $ managerSetProxy defaultProxy mySettings
+  manager <- newManager $ managerSetProxy (useProxy $ Proxy "127.0.0.2" 8080) mySettings
+--  manager <- newManager mySettings
     
   request <- parseRequest "https://httpbin.org/get"
   response <- httpLbs request manager
