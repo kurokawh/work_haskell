ff939651a154f38cad59577dc785b703485b2eae
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-02-28 00:25:57 +0900

    update for blog entry.

--- etc/network/http-client/blog/2-manager.hs
+++ etc/network/http-client/blog/2-manager.hs
@@ -1,23 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.HTTP.Client
 import Network.HTTP.Types.Status (statusCode)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Client.TLS (tlsManagerSettings) -- 新たにimport文を追加
+
 
 import Network.HTTP.Client.Internal
-mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 60000000 }
---mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 5000000 }
+-- tlsManagerSettingsをベースにmanagerResponseTimeoutをデフォルトの30秒から5秒に変更
+mySettings :: ManagerSettings
+mySettings = tlsManagerSettings { managerResponseTimeout = responseTimeoutMicro 5000000 } 
+
 
 main :: IO ()
 main = do
+  -- manageSetProxyでデフォルトのプロキシ設定を上書く。
+  -- tlsManagerSettingsを利用するとこでhttps通信が可能になる。
+  manager <- newManager $ managerSetProxy (useProxy $ Proxy "127.0.0.1" 8080) tlsManagerSettings
+
+
 --  manager <- newManager tlsManagerSettings
-  
---  manager <- newManager $ managerSetProxy noProxy mySettings
---  manager <- newManager $ managerSetProxy defaultProxy mySettings
-  manager <- newManager $ managerSetProxy (useProxy $ Proxy "127.0.0.2" 8080) mySettings
+--  manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
+--  manager <- newManager $ managerSetProxy defaultProxy tlsManagerSettings
 --  manager <- newManager mySettings
-    
-  request <- parseRequest "https://httpbin.org/get"
+
+
+  request <- parseRequest "https://httpbin.org/get" -- https通信に変更
   response <- httpLbs request manager
 
   putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
   print $ responseBody response
+
+
