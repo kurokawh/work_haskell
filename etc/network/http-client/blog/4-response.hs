b26e5280cc9d1ba1f9a8fee68ab6d3e395eacb83
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2018-02-28 14:25:47 +0900

    withRespoinse & brRead work fine now.

--- etc/network/http-client/blog/4-response.hs
+++ etc/network/http-client/blog/4-response.hs
@@ -8,14 +8,29 @@ main = do
 
   request <- parseRequest "http://httpbin.org/get"
 --  response <- httpLbs request manager
-  response <- withResponse request manager receiveResponse
---  response <- withResponse request manager brRead
+--  response <- withResponse request manager receiveResponse
+  withResponse request manager receiveResponse
+--  response <- withResponse request manager res
 
+{--
   putStrLn $ "response version: " ++ (show $ responseVersion response)
   putStrLn $ "status code: " ++ (show $ statusCode $ responseStatus response)
   putStrLn $ "response header: " ++ (show $ responseHeaders response)
   putStrLn $ "response body: " ++ (show $ responseBody response)
+--}
+--  return ()
+
+--receiveResponse :: Response BodyReader -> IO Data.ByteString.Internal.ByteString
+receiveResponse response = do
+  putStrLn $ "response version: " ++ (show $ responseVersion response)
+  putStrLn $ "status code: " ++ (show $ statusCode $ responseStatus response)
+  putStrLn $ "response header: " ++ (show $ responseHeaders response)
+--  putStrLn $ "response body: " ++ (show $ responseBody response)
+  bs <- brRead $ responseBody response
+  putStrLn $ "response body: " ++ (show bs)
+  return ()
+
+--res x = undefined
 
 
-receiveResponse (Response br) = do
-  return $ brRead br
+-- https://stackoverflow.com/questions/36733472/how-to-store-a-functions-return-argument-in-haskell
