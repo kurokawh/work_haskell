b83d943e8baad52dfa0c3856f37acba21058bb91
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-03-02 01:19:49 +0900

    add exception sample.

---
+++ etc/network/http-client/blog/5-exception.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Network.HTTP.Client
+import Network.HTTP.Types.Status (statusCode)
+import Control.Exception (try)
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+{--
+main :: IO ()
+main = do
+  manager <- newManager defaultManagerSettings
+
+  request <- parseRequest "http://httpbin.org/get"
+  eResponse <- try $ httpLbs request manager
+
+  case eResponse of
+    Left e -> print (e :: HttpException)
+    Right response -> do
+       putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+       print $ responseBody response
+--}
+
+main :: IO ()
+main = do
+    manager <- newManager defaultManagerSettings
+    eresponse <- try $ httpLbs "http://does-not-exist" manager
+
+    case eresponse of
+        Left e -> print (e :: HttpException)
+        Right response -> L8.putStrLn $ responseBody response
+
